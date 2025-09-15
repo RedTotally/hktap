84bd9964e3f9c71dddea8e366d38ad3b44c0e742
TheRed <94051573+RedTotally@users.noreply.github.com>
2025-09-15 19:35:29 +0800

    Disable rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,10 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
 export default [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     ignores: [
       "node_modules/**",
@@ -20,11 +14,11 @@ export default [
       "next-env.d.ts",
     ],
   },
-
-  ...compat.extends("plugin:react-hooks/recommended"),
   {
+
     rules: {
-      "react-hooks/rules-of-hooks": "off",
+      "no-undef": "off",
+      "no-unused-vars": "off",
     },
   },
 ];
