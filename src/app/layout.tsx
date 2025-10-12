a79c7f45715a928038c2178bb5e1f6dd971c764c
TheRed <94051573+RedTotally@users.noreply.github.com>
2025-10-12 11:24:50 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 import "leaflet/dist/leaflet.css";
+import Script from "next/script";
 
 const montserrat = Montserrat({
   variable: "--font-montserrat",
@@ -20,6 +21,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+          <Script
+            strategy="afterInteractive"
+            src="https://www.googletagmanager.com/gtag/js?id=G-9MBKJM86QB"
+          />
+          <Script
+            id="google-analytics"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-9MBKJM86QB');
+          `,
+            }}
+          />
+      </head>
       <body
         className={`${montserrat.variable} antialiased`}
       >
